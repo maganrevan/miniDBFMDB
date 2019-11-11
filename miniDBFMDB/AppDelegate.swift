9d102d2b87327814bfe46c9fc40b00d7cac56115
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2019-11-11 20:48:09 +0100

    Initial Commit

---
+++ miniDBFMDB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  miniDBFMDB
+//
+//  Created by Magnus Kruschwitz on 11.11.19.
+//  Copyright © 2019 Magnus Kruschwitz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ miniDBFMDB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  miniDBFMDB
+//
+//  Created by Magnus Kruschwitz on 11.11.19.
+//  Copyright © 2019 Magnus Kruschwitz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
